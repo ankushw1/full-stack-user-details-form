4bf7dd80d1479ae03003a2af4347ac51dba7aa71
Ankush Waghmare <waghmareankush050@gmail.com>
2023-07-02 10:15:24 +0530

    cors adding

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const formRoutes = require('./routes/formRoutes');
+const cors = require('cors')
+
+app.use(cors(
+  {
+      origin: ["https://deploy-mern-frontend.vercel.app"],
+      methods: ["POST", "GET"],
+      credentials: true
+  }
+));
 
 app.use(express.json());
 app.use('/form', formRoutes);
