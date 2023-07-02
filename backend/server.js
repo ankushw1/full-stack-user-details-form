51ad27560c11ec5172babfcbefb7942c83043ecb
Ankush Waghmare <waghmareankush050@gmail.com>
2023-07-02 12:05:32 +0530

    with mysql completed

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(cors(
 app.use(express.json());
 app.use('/form', formRoutes);
 
-const PORT = 8081
+const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
